0f6f36591ce327f2aaa3bb2912edb0e70d739f75
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-02-24 23:15:52 +0530

    Added Code for storing the input data in an array and after save clear inputs

--- 3.MovieList/assets/scripts/app.js
+++ 3.MovieList/assets/scripts/app.js
@@ -3,7 +3,34 @@ const ADD_MOVIE_MODAL = document.getElementById("add-modal");
 const BACKDROP = document.getElementById("backdrop");
 const CANCLE_ADD_MOVIE_BTN = ADD_MOVIE_MODAL.querySelector(".btn--passive");
 const FINAL_ADD_MOVIE_BTN = ADD_MOVIE_MODAL.querySelector(".btn--success");
+const USER_INPUTS = ADD_MOVIE_MODAL.querySelectorAll("input");
 
+let Movies =[];
+
+
+const clearMovieInputs =()=>{
+    USER_INPUTS[0].value ='';
+    USER_INPUTS[1].value ='';
+    USER_INPUTS[2].value ='';
+}
+
+const addMovieHandler= ()=>{
+    const titleValue = USER_INPUTS[0].value;
+    const imageValue = USER_INPUTS[1].value;
+    const ratingValue = USER_INPUTS[2].value;
+    if(titleValue.trim() == '' || imageValue.trim() == '' || ratingValue.trim() =='' || +ratingValue < 1 || +ratingValue > 5){
+        alert("Please Enter Valid Values (Rating between 1 and 5).");
+        return false;
+    }
+    let newMovie ={
+        title:titleValue,
+        image:imageValue,
+        rating:ratingValue
+    }
+    Movies.push(newMovie);
+    toggleMovieModel();
+    clearMovieInputs();
+}
 
 const backdropClickHandler =()=>{
     toggleMovieModel();
@@ -23,3 +50,4 @@ const toggleMovieModel =()=>{
 ADD_MOVIE_BTN.addEventListener("click",toggleMovieModel);
 BACKDROP.addEventListener("click",backdropClickHandler);
 CANCLE_ADD_MOVIE_BTN.addEventListener("click",backdropClickHandler);
+FINAL_ADD_MOVIE_BTN.addEventListener("click",addMovieHandler);
