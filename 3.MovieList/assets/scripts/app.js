d12c618b114da9409ddc0ea0cf5b275e729351d3
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-02-25 01:52:34 +0530

    Code added for promt if delete or not

--- 3.MovieList/assets/scripts/app.js
+++ 3.MovieList/assets/scripts/app.js
@@ -6,15 +6,31 @@ const FINAL_ADD_MOVIE_BTN = ADD_MOVIE_MODAL.querySelector(".btn--success");
 const USER_INPUTS = ADD_MOVIE_MODAL.querySelectorAll("input");
 const ENTRY_SECTION = document.getElementById("entry-text");
 const LIST_ROOOT = document.getElementById("movie-list");
+const ASK_DELETE_MODAL = document.getElementById("delete-modal");
+const REJECT_DELETION = ASK_DELETE_MODAL.querySelector(".btn--passive");
+const ACCEPT_DELETION = ASK_DELETE_MODAL.querySelector(".btn--danger");
 let UNIQUE_ID =0;
 
 let Movies =[];
 
 
-const deleteMovieHandler=(movieId)=>{
+const ConformDeletionHandler=(movieId)=>{
+    deleteMovie(movieId);
+    rejectDeletionHabdler();
+}
+
+const deleteMovie=(movieId)=>{
     let index = Movies.findIndex((p)=>p.id == movieId);
-    Movies.splice(index,1);
     LIST_ROOOT.children[index].remove();
+    Movies.splice(index,1);
+    updateUI();
+}
+
+const deleteMovieHandler=(movieId)=>{
+    ASK_DELETE_MODAL.classList.add("visible");
+    toggleBackDrop();
+    REJECT_DELETION.addEventListener("click",rejectDeletionHabdler);
+    ACCEPT_DELETION.addEventListener("click",ConformDeletionHandler.bind(this,movieId));
 }
 
 const renderNewMovieElement =(title,imageUrl,rating,id)=>{
@@ -65,28 +81,41 @@ const addMovieHandler= ()=>{
         id:movieId
     }
     Movies.push(newMovie);
-    toggleMovieModel();
+    closeMovieModel();
     clearMovieInputs();
     renderNewMovieElement(titleValue,imageValue,ratingValue,movieId);
     updateUI();
 }
 
 const backdropClickHandler =()=>{
-    toggleMovieModel();
+    closeMovieModel();
+    ASK_DELETE_MODAL.classList.remove("visible");
 }
 
 const toggleBackDrop =()=>{
     BACKDROP.classList.toggle("visible");
 }
 
-const toggleMovieModel =()=>{
-    ADD_MOVIE_MODAL.classList.toggle("visible");
+const showMovieModal=()=>{
+    ADD_MOVIE_MODAL.classList.add("visible");
     toggleBackDrop();
+}
+
+const closeMovieModel =()=>{
+    ADD_MOVIE_MODAL.classList.remove("visible");
+    toggleBackDrop();
+
+}
+
+const rejectDeletionHabdler =()=>{
+    toggleBackDrop();
+    ASK_DELETE_MODAL.classList.remove("visible");
+
 }
 
 
 
-ADD_MOVIE_BTN.addEventListener("click",toggleMovieModel);
+ADD_MOVIE_BTN.addEventListener("click",showMovieModal);
 BACKDROP.addEventListener("click",backdropClickHandler);
 CANCLE_ADD_MOVIE_BTN.addEventListener("click",backdropClickHandler);
 FINAL_ADD_MOVIE_BTN.addEventListener("click",addMovieHandler);
