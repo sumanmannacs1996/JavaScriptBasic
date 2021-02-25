a1a2df84fd9a658dd0036576e3f559e9a37f9dc7
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-02-25 23:37:46 +0530

    Make the code all class base and moduler

--- 5.ShopingCart/assets/scripts/app.js
+++ 5.ShopingCart/assets/scripts/app.js
@@ -1,9 +1,4 @@
-class Product{
-    title="DEFAULT";
-    imageUrl;
-    price;
-    description;
-
+class PRODUCT{
     constructor(title,imageUrl,price,description){
         this.title = title;
         this.imageUrl = imageUrl;
@@ -12,34 +7,49 @@ class Product{
     }
 };
 
-const productList={
-    products:[
-        new Product("A Cycle","assets/image/Cycle.png",40000,"A Electric bicycle!!"),
-        new Product("A Motor Cycle","assets/image/Cycle.png",40000,"A Electric bicycle!!"),
-        new Product("A Car","assets/image/Car.jpeg",120000,"A cruiser moror bike!!")
-        ],
-    render(){
-        const RENDER_HOOK=document.getElementById("app");
-        let productList =document.createElement('ul');
-        productList.classList.add("product-list");
-        for(let product of this.products){
-            let productElement =document.createElement("li");
+class PRODUCT_ITEM{
+    constructor(product){
+        this.product = product;
+    }
+    renderProduct(){
+        let productElement =document.createElement("li");
             productElement.classList.add("product-item");
             productElement.innerHTML=`
             <div>
-                <img src="${product.imageUrl}" alt="${product.title}">
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
                 <div class="product-item__content">
-                    <h2>${product.title}</h2>
-                    <h3>₹${product.price}</h3>
-                    <p>${product.description}</p>
+                    <h2>${this.product.title}</h2>
+                    <h3>₹${this.product.price}</h3>
+                    <p>${this.product.description}</p>
                     <button>Add to Cart</button>
                 </div>
             </div>
             `;
-            productList.append(productElement);
+            return productElement;
+    }
+
+};
+
+class PRODUCT_LIST {
+    products =[
+        new PRODUCT("A Cycle","assets/image/Cycle.png",40000,"A Electric bicycle!!"),
+        new PRODUCT("A Motor Cycle","assets/image/Cycle.png",40000,"A Electric bicycle!!"),
+        new PRODUCT("A Car","assets/image/Car.jpeg",120000,"A cruiser moror bike!!")
+    ]
+    constructor(){}
+    
+    renderList(){
+        const RENDER_HOOK=document.getElementById("app");
+        let productList =document.createElement('ul');
+        productList.classList.add("product-list");
+        for(let item of this.products){
+            const product_item = new PRODUCT_ITEM(item);
+            const rendered_product = product_item.renderProduct();
+            productList.append(rendered_product);
         }
         RENDER_HOOK.append(productList);
-
     }
 };
-productList.render();
+
+let product_list = new PRODUCT_LIST();
+product_list.renderList();
